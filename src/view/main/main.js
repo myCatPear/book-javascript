d97456f48515ccf7480e6fda58080594fed13b0c
Денис Тавабилов <dagger1233@gmail.com>
2023-10-13 07:53:17 +0500

    add header

--- src/view/main/main.js
+++ src/view/main/main.js
@@ -1,15 +1,38 @@
 import { AbstractView } from "../../common/view.js";
+import onChange from "on-change";
+import { Header } from "../../components/header/header.js";
 
 export class MainView extends AbstractView {
-    constructor() {
+    state = {
+        list: [],
+        loading: false,
+        searchQuery:undefined,
+        offset:0,
+    }; 
+
+    constructor(appState) {
         super();
-        this.setTitle('Поиск книг')
+        this.appState = appState;
+        this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.setTitle('Поиск книг');
+    }
+
+    appStateHook(path) {
+        console.log(path)
+        if (path === 'favorites') {
+            (console.log(path))
+        }
     }
 
     render() {
         const main = document.createElement('div');
-        main.innerHTML = 'Тест'
         this.app.innerHTML = '',
         this.app.append(main)
+        this.renderHeader()
+    }
+
+    renderHeader() {
+        const header = new Header(this.appState).render();
+        this.app.prepend(header)
     }
 }
